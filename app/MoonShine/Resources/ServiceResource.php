589cfc5639ae1af68ce129382c85c9c938d09d66
Nikita V <swebs.sh@gmail.com>
2025-07-09 13:35:02 +0500

    Поправки

---
+++ app/MoonShine/Resources/ServiceResource.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MoonShine\Resources;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Service;
+use MoonShine\Laravel\Resources\ModelResource;
+use MoonShine\UI\Components\Layout\Box;
+use MoonShine\UI\Fields\ID;
+use MoonShine\Contracts\UI\FieldContract;
+use MoonShine\Contracts\UI\ComponentContract;
+use MoonShine\UI\Fields\Image;
+use MoonShine\UI\Fields\Text;
+use MoonShine\UI\Fields\Textarea;
+
+/**
+ * @extends ModelResource<Service>
+ */
+class ServiceResource extends ModelResource
+{
+    protected string $model = Service::class;
+
+    protected string $title = 'Услуги';
+
+    /**
+     * @return list<FieldContract>
+     */
+    protected function indexFields(): iterable
+    {
+        return [
+            ID::make()->sortable(),
+        ];
+    }
+
+    /**
+     * @return list<ComponentContract|FieldContract>
+     */
+    protected function formFields(): iterable
+    {
+        return [
+            Box::make([
+                ID::make(),
+                Text::make('Название', 'name')->required()->unescape(),
+                Textarea::make('Описание', 'description')->unescape(),
+                Image::make('Изображение', 'image')
+                    ->disk('public')
+                    ->dir('uploads/services'),
+            ])
+        ];
+    }
+
+    /**
+     * @return list<FieldContract>
+     */
+    protected function detailFields(): iterable
+    {
+        return [
+            ID::make(),
+        ];
+    }
+
+    /**
+     * @param Service $item
+     *
+     * @return array<string, string[]|string>
+     * @see https://laravel.com/docs/validation#available-validation-rules
+     */
+    protected function rules(mixed $item): array
+    {
+        return [];
+    }
+}
