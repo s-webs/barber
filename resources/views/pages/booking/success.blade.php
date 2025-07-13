bf3e2d4b7ed4ded92859115954a8519d113fa045
Nikita V <swebs.sh@gmail.com>
2025-07-13 14:46:14 +0500

    Добавил ссылку на главную

--- resources/views/pages/booking/success.blade.php
+++ resources/views/pages/booking/success.blade.php
@@ -9,7 +9,8 @@
         <div class="flex justify-center items-center h-full container mx-auto px-4 mt-4">
             <div
                 class="bg-[var(--color-halftone)] w-full h-[400px] flex items-center justify-center max-w-3xl rounded-[15px] p-4 flex-col overflow-hidden">
-                <div class="text-xl font-semibold">Ваша запись успешно создана.</div>
+                <a href="{{ route('home.index') }}" class="text-xl bg-green-700 text-white px-[15px] py-[7px] rounded-[7px] text-center">Вернуться на главную страницу</a>
+                <div class="text-xl font-semibold mt-[70px]">Ваша запись успешно создана.</div>
                 <div class="text-xl mt-[70px] bg-blue-900 text-white px-[15px] py-[7px] rounded-[7px] text-center">Проверить записи вы можете в Telegram боте <i class="ph ph-telegram-logo"></i></div>
             </div>
         </div>
