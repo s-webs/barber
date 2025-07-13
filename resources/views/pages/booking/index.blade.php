efb0d83ca9348662340906f6817f8500ae122b1f
Nikita V <swebs.sh@gmail.com>
2025-07-13 16:28:05 +0500

    Работаю над ботом

--- resources/views/pages/booking/index.blade.php
+++ resources/views/pages/booking/index.blade.php
@@ -19,7 +19,7 @@
             <a href="https://s-webs.kz" target="_blank">Готовое решение для барбершопа S-WEBS</a>
         </div>
 
-        <div class="flex-grow flex justify-center items-center px-4">
+        <div class="flex-grow flex justify-center items-center px-4 mt-2">
             <div
                 class="bg-[var(--color-halftone)] w-full max-w-3xl rounded-[15px] flex flex-col overflow-hidden">
                 <div x-data="bookingForm()" x-init="init()" x-effect="step === 6 && initPhoneMask()"
